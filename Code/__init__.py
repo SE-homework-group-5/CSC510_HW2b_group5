d61b070676862d84888b0405584ab8b943afb85a
antgad <anantgadodia.17@gmail.com>
2021-09-02 11:47:08 -0400

    added test case

--- Code/__init__.py
+++ Code/__init__.py
@@ -5,30 +5,7 @@ Created on Wed Sep  1 22:01:36 2021
 @author: anant
 """
 
-def fibo(n):
-    #This is a python code to compute the fibonacci series upto n terms
-    print("Fibonacci series")
-    
-    a=0
-    b=1
-    c=0
-    count=0
-    fib=[]
-    if n==0:
-        print("n should be >0")
-    elif n==1:
-        print("fibonacci series of ",str(n)," terms: ",str(a) )
-        fib.append(a)
-    else:
-        print("fibonacci series of ",str(n)," terms: ")
-        while count<n:
-            print(a)
-            fib.append(a)
-            c=a+b
-            a=b
-            b=c
-            count+=1
+from fibonacci import fibonacci
             
-def test_answer():
-  assert len(fibo(5))==5
+
  
--- test/test_cases.py
+++ test/test_cases.py
@@ -1,1 +1,6 @@
-
+import sys
+sys.path.append('/../code/')
+from fibonacci import fibonacci
+def test_answer():
+    print("testing")
+    assert len(fibonacci.fibo(5))==5
