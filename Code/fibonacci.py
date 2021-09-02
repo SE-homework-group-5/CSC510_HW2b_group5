47e29f4a05a73a59bcfa7ecbc13f5488022590b7
agadodi <agadodi@ncsu.edu>
2021-09-01 22:20:29 -0400

    added code

---
+++ Code/fibonacci.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  1 22:01:36 2021
+
+@author: anant
+"""
+
+print("Fibonacci series")
+n=int(input("Enter Nbr of Terms: "))
+a=0
+b=1
+c=0
+count=0
+if n==0:
+    print("n should be >0")
+elif n==1:
+    print("fibonacci series of ",str(n)," terms: ",str(a) )
+else:
+    print("fibonacci series of ",str(n)," terms: ")
+    while count<n:
+        print(a)
+        c=a+b
+        a=b
+        b=c
+        count+=1
