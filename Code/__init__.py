b4f22feb30004bf16a9da03039eb3d0721572c0f
antgad <37169203+antgad@users.noreply.github.com>
2021-09-01 23:30:04 -0400

    Update __init__.py

--- Code/__init__.py
+++ Code/__init__.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep  1 22:54:15 2021
+Created on Wed Sep  1 22:01:36 2021
 
 @author: anant
 """
-import fibonacci
+
+def fibo(n):
+    #This is a python code to compute the fibonacci series upto n terms
+    print("Fibonacci series")
+    
+    a=0
+    b=1
+    c=0
+    count=0
+    fib=[]
+    if n==0:
+        print("n should be >0")
+    elif n==1:
+        print("fibonacci series of ",str(n)," terms: ",str(a) )
+        fib.append(a)
+    else:
+        print("fibonacci series of ",str(n)," terms: ")
+        while count<n:
+            print(a)
+            fib.append(a)
+            c=a+b
+            a=b
+            b=c
+            count+=1
+            
+def test_answer():
+  assert len(fibo(5))==5
+ 
